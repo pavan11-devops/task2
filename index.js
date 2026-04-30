dc61d72aca2fd84796f266bdeda8aae0615f1b12
root <root@ip-172-31-90-178.ec2.internal>
2026-04-30 12:02:29 +0000

    updated index.js

--- index.js
+++ index.js
@@ -79,6 +79,6 @@ app.post("/book", (req, res) => {
   `);
 });
 
-app.listen(PORT, () => {
-  console.log(\`Server running on http://localhost:\${PORT}\`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
